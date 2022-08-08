f93acf074edcfc666bae7cb20193a89358842bad
epulix77 <110837612+epulix77@users.noreply.github.com>
2022-08-08 18:02:03 +0300

    Update liikkuminen.cs

--- liikkuminen.cs
+++ liikkuminen.cs
@@ -7,12 +7,9 @@ public class liikkuminen : MonoBehaviour
     public static bool isGround;
     Rigidbody2D body;
     public float speed = 5;
-    public float jumpForce = 7;
-    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
         body = GetComponent<Rigidbody2D>();
     }
 
@@ -33,7 +30,6 @@ public class liikkuminen : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        anim.SetBool("Run", Input.GetAxis("Horizontal") != 0);
 
     }
 }
